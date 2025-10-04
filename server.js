b2cda4aa6cc37bc85c1df1641132418ae22ab5ca
esteb <estebancan.32@gmail.com>
2025-10-04 13:29:27 -0600

    cuarto push

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ require("./app/routes/pago.routes")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5173;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
